6831133bbb28ccfe7d8ca669d7873d083c14f47d
yangshengtao <shengtao.yst@alibaba-inc.com>
2017-01-23 15:30:54 +0800

    update README

--- ios/WXImgLoaderImpl.m
+++ ios/WXImgLoaderImpl.m
@@ -1,34 +1,8 @@
-/**
- * Created by Weex.
- * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
- *
- * This source code is licensed under the Apache Licence 2.0.
- * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
- */
+//  Created by yangshengtao on 2017/1/23.
+//  Copyright © 2016年 taobao. All rights reserved.
 
 #import "WXImgLoaderImpl.h"
 #import <SDWebImage/UIImageView+WebCache.h>
-
-#define MIN_IMAGE_WIDTH 36
-#define MIN_IMAGE_HEIGHT 36
-
-#if OS_OBJECT_USE_OBJC
-#undef  WXDispatchQueueRelease
-#undef  WXDispatchQueueSetterSementics
-#define WXDispatchQueueRelease(q)
-#define WXDispatchQueueSetterSementics strong
-#else
-#undef  WXDispatchQueueRelease
-#undef  WXDispatchQueueSetterSementics
-#define WXDispatchQueueRelease(q) (dispatch_release(q))
-#define WXDispatchQueueSetterSementics assign
-#endif
-
-@interface WXImgLoaderImpl()
-
-@property (WXDispatchQueueSetterSementics, nonatomic) dispatch_queue_t ioQueue;
-
-@end
 
 @implementation WXImgLoaderImpl
 
